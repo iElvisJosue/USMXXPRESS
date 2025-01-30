0d8218660f1115f2b0feee651b73c7225d46898a
Elvis Josué <elvisjosue98@hotmail.com>
2025-01-30 16:40:47 -0600

    Se agrego "Preguntas frecuentes"

---
+++ src/helpers/ListaDePreguntasFrecuentes.js
@@ -0,0 +1,100 @@
+export const ListaDePreguntasFrecuentes = {
+  RASTREO: [
+    {
+      SVG: "FQARastreo",
+      AltIcono: {
+        es: "Icono de preguntas frecuentes sobre rastreo de paquetes",
+        en: "Icon of frequently asked questions about package tracking",
+      },
+      TituloIcono: {
+        es: "Icono de preguntas frecuentes sobre rastreo de paquetes",
+        en: "Icon of frequently asked questions about package tracking",
+      },
+      TituloPregunta: {
+        es: "¿Cómo puedo rastrear mi paquete?",
+        en: "How can I track my package?",
+      },
+      RespuestaPregunta: {
+        es: "Para rastrear tu paquete, ingresa el número de guía en el campo de búsqueda de nuestra página de rastreo. Este número lo puedes encontrar en tu ticket de confirmación de envío.",
+        en: "To track your package, enter the tracking number in the search field on our tracking page. You can find this number in your shipping confirmation ticket.",
+      },
+    },
+    {
+      SVG: "FQADisponibilidad",
+      AltIcono: {
+        es: "Icono de preguntas frecuentes sobre rastreo de paquetes",
+        en: "Icon of frequently asked questions about package tracking",
+      },
+      TituloIcono: {
+        es: "Icono de preguntas frecuentes sobre rastreo de paquetes",
+        en: "Icon of frequently asked questions about package tracking",
+      },
+      TituloPregunta: {
+        es: "¿Cuándo estará disponible la información de rastreo de mi paquete?",
+        en: "When will my package tracking information be available?",
+      },
+      RespuestaPregunta: {
+        es: "La información de rastreo estará disponible una vez que tu paquete haya sido registrado en el sistema y esté en tránsito. Esto suele ocurrir dentro de las 24 horas posteriores a la creación del pedido.",
+        en: "Tracking information will be available once your package has been registered in the system and is in transit. This usually happens within 24 hours of order creation.",
+      },
+    },
+    {
+      SVG: "FQARetraso",
+      Box: "0 0 14 14",
+      AltIcono: {
+        es: "Icono de preguntas frecuentes sobre retrasos en el rastreo de paquetes",
+        en: "Icon of frequently asked questions about package tracking delays",
+      },
+      TituloIcono: {
+        es: "Icono de preguntas frecuentes sobre retrasos en el rastreo de paquetes",
+        en: "Icon of frequently asked questions about package tracking delays",
+      },
+      TituloPregunta: {
+        es: "Mi paquete está retrasado, ¿qué debo hacer?",
+        en: "My package is delayed, what should I do?",
+      },
+      RespuestaPregunta: {
+        es: " Los retrasos pueden ocurrir debido a condiciones climáticas, alta demanda o problemas logísticos. Te recomendamos esperar 24-48 horas adicionales. Si el estado no cambia, contáctanos para obtener más información.",
+        en: "Delays may occur due to weather conditions, high demand, or logistical issues. We recommend waiting an additional 24-48 hours. If the status does not change, contact us for more information.",
+      },
+    },
+    {
+      SVG: "FQAError",
+      AltIcono: {
+        es: "Icono de preguntas frecuentes sobre errores en el rastreo",
+        en: "Icon of frequently asked questions about tracking errors",
+      },
+      TituloIcono: {
+        es: "Icono de preguntas frecuentes sobre errores en el rastreo",
+        en: "Icon of frequently asked questions about tracking errors",
+      },
+      TituloPregunta: {
+        es: "¿Por qué no funciona mi número de guía?",
+        en: "Why is my tracking number not working?",
+      },
+      RespuestaPregunta: {
+        es: "Asegúrate de ingresar correctamente el número de guía en el formato adecuado, verificando que tenga una longitud mínima de 6 caracteres y que, no incluya caracteres especiales como guiones (/), comas (,), espacios ( ) o puntos y comos (;). Si tu número de guía no funciona, contacta directamente al servicio de atención al cliente para obtener asistencia personalizada.",
+        en: "Make sure to enter the tracking number correctly in the proper format, ensuring it has a minimum length of 6 characters and does not include special characters such as slashes (/), commas (,), spaces ( ), or semicolons (;). If your tracking number does not work, contact the customer service team directly for personalized assistance.",
+      },
+    },
+    {
+      SVG: "FQAEntregado",
+      AltIcono: {
+        es: "Icono de preguntas frecuentes sobre paquetes marcados como entregados",
+        en: "Icon of frequently asked questions about packages marked as delivered",
+      },
+      TituloIcono: {
+        es: "Icono de preguntas frecuentes sobre paquetes marcados como entregados",
+        en: "Icon of frequently asked questions about packages marked as delivered",
+      },
+      TituloPregunta: {
+        es: "Mi paquete muestra 'Entregado', pero no lo he recibido. ¿Qué debo hacer?",
+        en: "My package shows 'Delivered', but I haven't received it. What should I do?",
+      },
+      RespuestaPregunta: {
+        es: "Primero, verifica si alguien más en tu domicilio lo recibió o si fue dejado en un área segura. Si no lo encuentras, contáctanos de inmediato con tu número de guía para iniciar una investigación.",
+        en: "First, make sure someone else in your home received it or it was left in a secure area. If you can't find it, contact us immediately with your tracking number to initiate a investigation.",
+      },
+    },
+  ],
+};
---
+++ src/translations/DiccionarioPreguntasFrecuentes.js
@@ -0,0 +1,22 @@
+export const DiccionarioPreguntasFrecuentes = {
+  AriaLabelSeccion: {
+    es: "Sección de preguntas frecuentes",
+    en: "FAQ section",
+  },
+  Titulo: {
+    es: "Preguntas Frecuentes",
+    en: "FQAs",
+  },
+  Descripcion: {
+    es: "¿Tienes preguntas sobre cómo rastrear tu paquete? Encuentra respuestas claras y detalladas a las dudas más comunes sobre el seguimiento de envíos, estados de paquetes, números de tracking y más. Optimiza tu experiencia de rastreo con nuestra guía completa. Si no encuentras lo que buscas, contáctanos para obtener ayuda personalizada. ¡Te ayudamos a mantener el control de tus envíos en todo momento! 🚚",
+    en: "Do you have questions about how to track your package? Find clear and detailed answers to the most common doubts about package tracking, shipment status, tracking numbers, and more. Optimize your tracking experience with our complete guide. If you don’t find what you’re looking for, contact us for personalized assistance. We’re here to help you stay in control of your shipments every step of the way! 🚚",
+  },
+  PreguntaUno: {
+    es: "¿Cómo puedo rastrear mi paquete?",
+    en: "How can I track my package?",
+  },
+  RespuestaUno: {
+    es: "Para rastrear tu paquete, simplemente ingresa el número de seguimiento en el campo de entrada de tu navegador o en el formulario de rastreo de paquetes.",
+    en: "To track your package, simply enter the tracking number in the input field of your browser or in the package tracking form.",
+  },
+};
