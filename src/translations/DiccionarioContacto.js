1cff3a03eeb62aab760d2def692ab131e49139ba
Elvis Josué <elvisjosue98@hotmail.com>
2025-02-25 13:20:18 -0600

    Se corrigio el robots

--- src/translations/DiccionarioContacto.js
+++ src/translations/DiccionarioContacto.js
@@ -8,7 +8,7 @@ export const DiccionarioContacto = {
     FormularioTexto:
       "Dejanos tus datos y cuéntanos como podemos ser parte de tu próximo envío. En cada paquete, llevamos tu confianza y nuestro compromiso.",
     FormularioAriaLabel: "Formulario de contacto de USMX",
-    FormulatioBotonTitulo: "Enviar mensaje a USMX Express",
+    FormulatioBotonTitulo: "Enviar mensaje a USMXXPRESS",
     FormularioBoton: "Enviar mensaje",
   },
   en: {
@@ -20,7 +20,7 @@ export const DiccionarioContacto = {
     FormularioTexto:
       "Leave your details and tell us how we can be part of your next shipment. In each package, we carry your trust and our commitment.",
     FormularioAriaLabel: "Contact form of USMX",
-    FormulatioBotonTitulo: "Send message to USMX Express",
+    FormulatioBotonTitulo: "Send message to USMXXPRESS",
     FormularioBoton: "Send Message",
   },
 };
